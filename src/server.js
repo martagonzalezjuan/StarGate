559c3de1ca0ffc50544c5c463eb84a3086c321f8
kernel-45 <ikergarciadeleon@gmail.com>
2025-02-25 23:28:32 +0100

    Create server.js

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000; // Usa el puerto 3000 o el que definas en la variable de entorno PORT
+
+// Servir archivos estáticos desde la carpeta "public"
+app.use(express.static('public'));
+
+// Ruta para enviar el archivo index.html (opcional, ya que express.static lo servirá automáticamente)
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en el puerto ${port}`);
+});
