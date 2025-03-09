977d2dbf8b406584bfe5f24649bf05c3753ca0d6
martagonzalezjuan <marta.gonzalez11@estudiant.uib.cat>
2025-03-09 13:16:04 +0100

    carousell on the side

--- src/VideoGallery.js
+++ src/VideoGallery.js
@@ -1,11 +1,9 @@
-// VideoGallery.js
 import React, { useState } from 'react';
 import CustomVideoPlayer from './CustomVideoPlayer';
 import './VideoGallery.css';
 
 function VideoGallery({ videos }) {
   const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
-
   const currentVideo = videos && videos.length > 0 ? videos[currentVideoIndex] : null;
 
   const handleVideoSelect = (index) => {
@@ -26,24 +24,23 @@ function VideoGallery({ videos }) {
           <CustomVideoPlayer key={currentVideo.id} videoData={currentVideo} />
           <h3>{currentVideo.title}</h3>
         </div>
-      </div>
-
-      <div className="video-slider">
-        {videos.map((video, index) => (
-          <div
-            key={video.id}
-            className={`slider-item ${index === currentVideoIndex ? 'active' : ''}`}
-            onClick={() => handleVideoSelect(index)}
-          >
-            <video muted>
-              <source
-                src={`${process.env.PUBLIC_URL}/assets/video${video.id}/video${video.id}_720p.mp4`}
-                type="video/mp4"
-              />
-            </video>
-            <p>{video.title}</p>
-          </div>
-        ))}
+        <div className="video-slider">
+          {videos.map((video, index) => (
+            <div
+              key={video.id}
+              className={`slider-item ${index === currentVideoIndex ? 'active' : ''}`}
+              onClick={() => handleVideoSelect(index)}
+            >
+              <video muted>
+                <source
+                  src={`${process.env.PUBLIC_URL}/assets/video${video.id}/video${video.id}_720p.mp4`}
+                  type="video/mp4"
+                />
+              </video>
+              <p>{video.title}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
